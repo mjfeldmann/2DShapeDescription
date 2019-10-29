82d95de9b0f7800d79bea5f5e787d8a5c559f82f
Mitchell Feldmann <mjfeldmann@ucdavis.edu>
2019-10-29 13:01:58 -0700

    Edits to EFA and GPA R scripts

--- R_scripts/EFA_Momocs.R
+++ R_scripts/EFA_Momocs.R
@@ -14,10 +14,11 @@ lf <- list.files("~/Desktop/Reading/Strawberry_Research/Heterosis/Salinas/Data/I
 #Certain images seem to crash the import_jpg() function.
 coo <- import_jpg(lf[-c(471,2880,2895,3283,3752,3940,3963,4362,5362,5367,5485,1686,2274,2689,3616,3860,4408,4763,4764,5089,5474,5817,5821,6284)])
 
-#Modifying the threshold for each image seemed to work. 
+coo <- import_jpg(lf[-c(471,2880,2895,3283,3752,3940,3963,4362,5362,5367,5485,1686,2274,2689,3616,3860,4408,4763,4764,5089,5474,5817,5821,6284)])
+
 coo.0 = import_jpg(lf[1686],threshold = 0.001)
 coo.1 = import_jpg(lf[2274],threshold = 0.001)
-coo.2 = import_jpg(lf[2689],threshold = 0.001)
+coo.2 = import_jpg(lf[2689],threshold = 0.01)
 coo.3 = import_jpg(lf[3616],threshold = 0.015)
 coo.4 = import_jpg(lf[3860],threshold = 0.015)
 coo.5 = import_jpg(lf[4408],threshold = 0.001)
@@ -27,15 +28,15 @@ coo.8 = import_jpg(lf[5089],threshold = 0.005)
 coo.9 = import_jpg(lf[5474],threshold = 0.005)
 coo.10 = import_jpg(lf[5817],threshold = 0.005)
 coo.11 = import_jpg(lf[5821],threshold = 0.005)
-coo.12 = import_jpg(lf[6284],threshold = 0.01)
+coo.12 = import_jpg(lf[6284])
 coo.13 = import_jpg(lf[471],threshold = 0.01)
 coo.14 = import_jpg(lf[2880],threshold = 0.02)
 coo.15 = import_jpg(lf[2895],threshold = 0.01)
 coo.16 = import_jpg(lf[3283],threshold = 0.001)
-coo.17 = import_jpg(lf[3752],threshold = 0.02)
+coo.17 = import_jpg(lf[3752],threshold = 0.015)
 coo.18 = import_jpg(lf[3940],threshold = 0.001)
 coo.19 = import_jpg(lf[3963],threshold = 0.01)
-coo.20 = import_jpg(lf[4362],threshold = 0.01)
+coo.20 = import_jpg(lf[4362])
 coo.21 = import_jpg(lf[5362],threshold = 0.01)
 coo.22 = import_jpg(lf[5367],threshold = 0.01)
 coo.23 = import_jpg(lf[5485],threshold = 0.001)
@@ -64,6 +65,24 @@ coo = append(coo,coo.20)
 coo = append(coo,coo.21)
 coo = append(coo,coo.22)
 coo = append(coo,coo.23)
+
+#l=c()
+#for(i in 1:6874){
+#  l[i] = dim(coo[[i]])[1]
+#}
+#plot(l)
+
+#If using modified K means
+#KM.dat <- read.csv("~/Box/Desktop/Reading/Strawberry_Research/Heterosis/Salinas/Data/2018_Salinas_Fruit_Morph_KMA.csv")
+#k8 <- KM.dat[which(KM.dat$K8 == 6),]
+#
+#coo.rev = which(names(coo) %in% k8$label.z)
+#
+#for(i in coo.rev){
+#  print(i)
+#  coo[[i]][,1] = max(coo[[i]][,1]) - coo[[i]][,1]
+#  coo[[i]] = apply(coo[[i]],2,rev)
+#}
 
 strw <- Out(coo)
 
--- R_scripts/GPA_Momocs.R
+++ R_scripts/GPA_Momocs.R
@@ -15,7 +15,7 @@ coo <- import_jpg(lf[-c(471,2880,2895,3283,3752,3940,3963,4362,5362,5367,5485,16
 
 coo.0 = import_jpg(lf[1686],threshold = 0.001)
 coo.1 = import_jpg(lf[2274],threshold = 0.001)
-coo.2 = import_jpg(lf[2689],threshold = 0.001)
+coo.2 = import_jpg(lf[2689],threshold = 0.01)
 coo.3 = import_jpg(lf[3616],threshold = 0.015)
 coo.4 = import_jpg(lf[3860],threshold = 0.015)
 coo.5 = import_jpg(lf[4408],threshold = 0.001)
@@ -25,15 +25,15 @@ coo.8 = import_jpg(lf[5089],threshold = 0.005)
 coo.9 = import_jpg(lf[5474],threshold = 0.005)
 coo.10 = import_jpg(lf[5817],threshold = 0.005)
 coo.11 = import_jpg(lf[5821],threshold = 0.005)
-coo.12 = import_jpg(lf[6284],threshold = 0.01)
+coo.12 = import_jpg(lf[6284])
 coo.13 = import_jpg(lf[471],threshold = 0.01)
 coo.14 = import_jpg(lf[2880],threshold = 0.02)
 coo.15 = import_jpg(lf[2895],threshold = 0.01)
 coo.16 = import_jpg(lf[3283],threshold = 0.001)
-coo.17 = import_jpg(lf[3752],threshold = 0.02)
+coo.17 = import_jpg(lf[3752],threshold = 0.015)
 coo.18 = import_jpg(lf[3940],threshold = 0.001)
 coo.19 = import_jpg(lf[3963],threshold = 0.01)
-coo.20 = import_jpg(lf[4362],threshold = 0.01)
+coo.20 = import_jpg(lf[4362])
 coo.21 = import_jpg(lf[5362],threshold = 0.01)
 coo.22 = import_jpg(lf[5367],threshold = 0.01)
 coo.23 = import_jpg(lf[5485],threshold = 0.001)
@@ -62,6 +62,23 @@ coo = append(coo,coo.20)
 coo = append(coo,coo.21)
 coo = append(coo,coo.22)
 coo = append(coo,coo.23)
+
+#l=c()
+#for(i in 1:6874){
+#  l[i] = dim(coo[[i]])[1]
+#}
+#plot(l)
+#
+#KM.dat <- read.csv("~/Box/Desktop/Reading/Strawberry_Research/Heterosis/Salinas/Data/2018_Salinas_Fruit_Morph_KMA.csv")
+#k8 <- KM.dat[which(KM.dat$K8 == 6),]
+#
+#coo.rev = which(names(coo) %in% k8$label.z)
+#
+#for(i in coo.rev){
+#  print(i)
+#  coo[[i]][,1] = max(coo[[i]][,1]) - coo[[i]][,1]
+#  coo[[i]] = apply(coo[[i]],2,rev)
+#}
 
 coo2 <- list()
 
